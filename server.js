0e48f450d789a58abce3fe11c9aab3ae4b5b7b1c
rohit-verma-au7 <58546247+rohit-verma-au7@users.noreply.github.com>
2021-02-08 20:04:53 +0530

    Deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const PORT = process.env.PORT || 3001;
+const path = require("path");
 
 const app = express();
 
@@ -14,15 +15,21 @@ app.use(
   })
 );
 
-app.get("/", (req, res) => {
-  res.send("API running");
-});
-
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+
+// Serve static assest in production
+if (process.env.NODE_ENV === "production") {
+  // SEt static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
